f03873757d3e1105fdf7a7aea2881c54cd04c72d
daniel mcnamara <danmac8407@gmail.com>
2020-01-14 17:07:52 -0600

    completed variables,mouseover, and keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+//variables
+const body = document.querySelector('body');
+const destinationImg = document.querySelector('.content-destination img');
+const btn = document.querySelectorAll('.btn');
+const destinationDivs = document.querySelectorAll('.destination');
+const nav = document.querySelectorAll('nav a');
+const mainNav = document.querySelector('.main-navigation');
+const mouseLeave = document.querySelector('.intro h2');
+
+// 1. mouseover
+destinationDivs.forEach(function(element) {
+	element.addEventListener('mouseover', function() {
+		element.style.color = 'red';
+	});
+});
+
+// 1. keydown
+body.addEventListener('keydown', function() {
+	this.style.backgroundColor = 'skyblue';
+});
+
